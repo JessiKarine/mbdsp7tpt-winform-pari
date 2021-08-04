f3fde73614390e2ffd62ca86141fa42c949b79ab
@mevaElodie <rrmeva@yahoo.com>
2021-08-04 03:00:43 +0300

    Initial commit

---
+++ ListeJoueurs.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace ParisWinform
+{
+    partial class ListeJoueurs
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(-2, -2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(809, 456);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(353, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Liste des joueurs";
+            // 
+            // ListeJoueurs
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "ListeJoueurs";
+            this.Text = "ListeJoueurs";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ StartPage.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+namespace ParisWinform
+{
+    partial class StartPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.listeDesJoueursToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mouToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.débiterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.créditerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.déconnexionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.profilToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.déconnexionToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.listeDesJoueursToolStripMenuItem,
+            this.mouToolStripMenuItem,
+            this.déconnexionToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // listeDesJoueursToolStripMenuItem
+            // 
+            this.listeDesJoueursToolStripMenuItem.Name = "listeDesJoueursToolStripMenuItem";
+            this.listeDesJoueursToolStripMenuItem.Size = new System.Drawing.Size(106, 20);
+            this.listeDesJoueursToolStripMenuItem.Text = "Liste des joueurs";
+            // 
+            // mouToolStripMenuItem
+            // 
+            this.mouToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripSeparator1,
+            this.débiterToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.créditerToolStripMenuItem});
+            this.mouToolStripMenuItem.Name = "mouToolStripMenuItem";
+            this.mouToolStripMenuItem.Size = new System.Drawing.Size(89, 20);
+            this.mouToolStripMenuItem.Text = "Mouvements";
+            // 
+            // débiterToolStripMenuItem
+            // 
+            this.débiterToolStripMenuItem.Name = "débiterToolStripMenuItem";
+            this.débiterToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.débiterToolStripMenuItem.Text = "Débiter";
+            // 
+            // créditerToolStripMenuItem
+            // 
+            this.créditerToolStripMenuItem.Name = "créditerToolStripMenuItem";
+            this.créditerToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.créditerToolStripMenuItem.Text = "Créditer";
+            // 
+            // déconnexionToolStripMenuItem
+            // 
+            this.déconnexionToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.profilToolStripMenuItem,
+            this.toolStripSeparator3,
+            this.déconnexionToolStripMenuItem1});
+            this.déconnexionToolStripMenuItem.Name = "déconnexionToolStripMenuItem";
+            this.déconnexionToolStripMenuItem.Size = new System.Drawing.Size(102, 20);
+            this.déconnexionToolStripMenuItem.Text = "Profil utilisateur";
+            this.déconnexionToolStripMenuItem.Click += new System.EventHandler(this.déconnexionToolStripMenuItem_Click);
+            // 
+            // profilToolStripMenuItem
+            // 
+            this.profilToolStripMenuItem.Name = "profilToolStripMenuItem";
+            this.profilToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.profilToolStripMenuItem.Text = "Profil";
+            // 
+            // déconnexionToolStripMenuItem1
+            // 
+            this.déconnexionToolStripMenuItem1.Name = "déconnexionToolStripMenuItem1";
+            this.déconnexionToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.déconnexionToolStripMenuItem1.Text = "Déconnexion";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(177, 6);
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(177, 6);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem listeDesJoueursToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mouToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem débiterToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem créditerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem déconnexionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem profilToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem déconnexionToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+    }
+}
+
